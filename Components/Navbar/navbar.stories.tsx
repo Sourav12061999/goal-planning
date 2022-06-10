c98dae6a992a1ee35f317af8fdb08a356d29a948
Sourav12061999 <sdunity12061999@gmail.com>
2022-06-10 22:12:17 +0530

    Initial Commit

---
+++ Components/Navbar/navbar.stories.tsx
@@ -0,0 +1,8 @@
+import Appstructure from "./navbar";
+export default {
+    title: "Independent Component/AppStructure",
+    component: Appstructure,
+};
+export const  Basic = () => (
+    <Appstructure/>
+)
---
+++ Components/Navbar/navbar.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+import useStyles from "./styles";
+
+export default function Appstructure() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  const { classes } = useStyles();
+  return (
+    <AppShell
+      className={classes.main}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      fixed
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
+      header={
+        <Header height={70} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Application header</Text>
+          </div>
+        </Header>
+      }
+    >
+      <Text>Resize app to see responsive navbar in action</Text>
+    </AppShell>
+  );
+}
